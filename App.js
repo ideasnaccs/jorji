b2736bb22c598159c8a925aa94baac7aac03f5f3
Paul Zhang <paulz@ucla.edu>
2023-01-21 02:07:31 -0800

    Add UI Kitten library and Eva Colors theme

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as eva from "@eva-design/eva";
 import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
+import theme from "./theme.json";
 
 import Studying from "./screens/Studying";
 import Chores from "./screens/Chores";
@@ -12,7 +13,7 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
+    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
       <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen name="Studying" component={Studying}></Tab.Screen>
