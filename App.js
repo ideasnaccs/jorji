25e6e4b711ed777f41414dd3b97eebfa84b14b2b
Paul Zhang <paulz@ucla.edu>
2023-01-21 01:03:42 -0800

    Add UI Kitten

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
 
 import Studying from "./screens/Studying";
 import Chores from "./screens/Chores";
@@ -10,13 +12,15 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Studying" component={Studying}></Tab.Screen>
-        <Tab.Screen name="Chores" component={Chores}></Tab.Screen>
-        <Tab.Screen name="Exercise" component={Exercise}></Tab.Screen>
-        <Tab.Screen name="Self Care" component={SelfCare}></Tab.Screen>
-      </Tab.Navigator>
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Studying" component={Studying}></Tab.Screen>
+          <Tab.Screen name="Chores" component={Chores}></Tab.Screen>
+          <Tab.Screen name="Exercise" component={Exercise}></Tab.Screen>
+          <Tab.Screen name="Self Care" component={SelfCare}></Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
